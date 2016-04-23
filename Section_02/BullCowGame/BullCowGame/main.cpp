600897f52d0974c9796be575715950567f6690b2
finian826 <tmacmill@gmail.com>
2016-04-22 20:40:32 -0400

    Changes
    Some mods to the program

--- Section_02/BullCowGame/BullCowGame/main.cpp
+++ Section_02/BullCowGame/BullCowGame/main.cpp
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <iostream>
 #include "4wordlist.h"
 #include "5wordlist.h"
+
+using namespace std;
 
 
 int main()
 {
 	int RetVal = 0;
+	constexpr int WORD_LENGTH = 6;
+
+	cout << "Welcome to Bulls and Cows\n";
+	cout << endl;
+	cout << "Can you guess and " << WORD_LENGTH << " letter isogram I'm thinking of? \n";
+	cout << endl;
 
 	return RetVal;
 }
