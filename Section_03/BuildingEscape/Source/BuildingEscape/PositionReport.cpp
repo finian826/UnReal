3a20c7a86c1aafc8f3c797b48d33400b80f69f10
finian826 <tmacmill@gmail.com>
2016-04-27 23:10:55 -0400

    Start of Unreal
    Starting Unreal work.

---
+++ Section_03/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,33 @@
+// Copyright MacMillan Programming Services 2016
+#include "BuildingEscape.h"
+#include "PositionReport.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s!\n"),*ObjectName);
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
